253cee749391192227aceec2f1dd2da9dd0c5ad9
Nathnael Chanyalew <nathnaelc5@gmail.com>
2024-06-18 15:05:30 -0400

    Add the logic for the computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("h")
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if(randomNumber == 1){
+        return "rock";
+    }else if(randomNumber == 2){
+        return "paper";
+    }
+    return "scissors";
+}
+
+console.log(getComputerChoice());
